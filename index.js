4eea63682b881bbe8df70b343f6b3284af0ee8f6
jeremyrrose <jeremy.r.rose@gmail.com>
2021-03-17 23:55:57 -0400

    updated instructions on npm start

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ Choose a sort to display, along with optional command line arguments:
 Syntax: npm run SORT [TIMEOUT [WAVE INPUT_ARRAY]]
 \x1b[38;2;198;255;198m
 Sorts: bubble insertion merge quick
-Timeout: First argument after sort (default : 250)
+Timeout: First argument after sort (default : 400)
 Options: 
 wave (true if included)
 words nums letters (sets input array, default : letters)
 \x1b[38;2;198;198;255m
 Examples:
-npm run bubble
+npm run quick
 npm run merge 50 wave words
 
 
