0d9665ea49421936a72a2e787cedf667150238ce
jeremyrrose <jeremy.r.rose@gmail.com>
2021-03-19 02:13:25 -0400

    updated index.js directions

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const message = `\x1b[38;2;255;198;198m
 Choose a sort to display, along with optional command line arguments:
 
-Syntax: npm run SORT [TIMEOUT [WAVE INPUT_ARRAY]]
+Syntax: npm run SORT [ TIMEOUT [ OPTIONS ] ]
 \x1b[38;2;198;255;198m
 Sorts: bubble insertion merge quick
 Timeout: First argument after sort (default : 400)
 Options: 
-wave (true if included)
+wave notes (both boolean; each true if included)
 words nums letters (sets input array, default : letters)
 \x1b[38;2;198;198;255m
 Examples:
 npm run quick
 npm run merge 50 wave words
 
+View README.md or visit https://github.com/jeremyrrose/rainbow-sorts for more details.
 
 `
 
