a4a8a6b813c38d9d6e21288c9989b4afeecb65c0
jeremyrrose <jeremy.r.rose@gmail.com>
2021-03-19 16:46:07 -0400

    mac term compatibility

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const message = `\x1b[38;2;255;198;198m
+const message = `\x1b[38;5;224m
 Choose a sort to display, along with optional command line arguments:
 
 Syntax: npm run SORT [ TIMEOUT [ OPTIONS ] ]
-\x1b[38;2;198;255;198m
+\x1b[38;5;50m
 Sorts: bubble insertion merge quick
 Timeout: First argument after sort (default : 400)
 Options: 
 wave notes (both boolean; each true if included)
 words nums letters (sets input array, default : letters)
-\x1b[38;2;198;198;255m
+\x1b[38;5;45m
 Examples:
 npm run quick
 npm run merge 50 wave words
